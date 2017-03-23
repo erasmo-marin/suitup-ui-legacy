070d87e380f2b2d0979a023f169629d9129b5b2c
Erasmo Marín <erasmo.marin@gmail.com>
2017-03-23 01:09:12 -0300

    add soul

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
-import '../src/styles/index.less';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+import "../src/styles/index.less";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-  constructor(props) {
-    super(props);
-  }
+    componentDidMount() {
+        console.log(
+            `%c
 
-  render () {
-    return (<Routes/>);
-  }
+                      #@@&*,          .*%@@*                   
+                     &@@@@@@@@@@%*%@@@@@@@@@#                  
+                    @@@@@@@@@@@@@/@@@@@@@@@@@@                 
+                    @@@@@@@@@@@@/,/@@@@@@@@@@@                 
+                     &@@@@@@@@@%,,,(@@@@@@@@@                  
+                      @@@@@@@@&*,,,,%@@@@@@@                   
+                       @@@@@@@@/,,,*@@@@@@@                    
+                        @@@@@@@*,,,*@@@@@@@                    
+                        @@@@@@@*,,,,&@@@@@                     
+                         @@@@@&,,,,,&@@@@                      
+                          @@@@%,,,,,%@@@@                      
+                          @@@@#,,,,,#@@&                       
+                           @@@(,,,,,(@@                        
+                            @@/,,,,,/@                         
+                             @*,,,,,*@                         
+                              ,,,,,,/                          
+                              /,,,,*                           
+                               /,,,                            
+                                *,                             
+                                 ,                             
 
+             ____        _ _   _   _         _   _ ___ 
+           / ___| _   _(_) |_| | | |_ __   | | | |_ _|
+           \\___ \\| | | | | __| | | | '_ \\  | | | || | 
+            ___) | |_| | | |_| |_| | |_) | | |_| || | 
+           |____/ \\__,_|_|\\__|\\___/| .__/   \\___/|___|
+                                   |_|                
+`, 'color: #41c39f');
+    }
+
+    render() {
+        return <Routes />;
+    }
 }
 
 export default App;
